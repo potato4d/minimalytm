e50c1f79497c730bbfdb5a51bcc7b2c3fea02dd4
HANATANI Takuma <mail@potato4d.me>
2019-05-09 23:28:12 +0900

    v0.3.2

--- src/ui/contextTemplate.ts
+++ src/ui/contextTemplate.ts
@@ -3,6 +3,10 @@ import { App, MenuItemConstructorOptions } from 'electron'
 export function createContextTemplate(app: App): MenuItemConstructorOptions[] {
   return [
     {
+      label: 'About this application',
+      selector: 'orderFrontStandardAboutPanel:'
+    } as any,
+    {
       label: 'Quit',
       accelerator: 'Command+Q',
       click: () => {
